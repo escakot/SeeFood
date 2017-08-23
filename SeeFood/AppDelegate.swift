a7947fe77324d70864b8f887c73f4b57aef71238
Thiago Hissa <thiagohissa@gmail.com>
2017-08-23 13:46:33 -0400

    Added TableView to DetailController

--- SeeFood/AppDelegate.swift
+++ SeeFood/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
   
-  let googlePlacesAPIkey = "AIzaSyB8WnwQLN2IMPvdBgOWgy-yzSRsTKmhTkY"
+  let googlePlacesAPIkey = "AIzaSyCx0rf6xJuIyWgY_OlcdQtatCZv9aZwWiY"
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
